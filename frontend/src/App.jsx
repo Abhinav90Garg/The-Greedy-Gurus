000af45bc0c1576ad25e8c18ba655caca914b7c7
Gurleen404 <gurleenkaur07150@gmail.com>
2026-04-06 22:07:41 +0530

    error handling imported react mor ethan once

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
