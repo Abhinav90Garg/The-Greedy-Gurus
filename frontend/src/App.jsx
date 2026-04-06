b164dcc1334e5012659da13bea6f0deaf84c3747
Gurleen404 <gurleenkaur07150@gmail.com>
2026-04-06 14:16:48 +0530

    feat: add /dashboard route for ChatTutor component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from "./components/LandingPage.jsx";
-
-
-// import Auth from "./components/Auth.jsx";
-// import ChatTutor from "./components/ChatTutor.jsx";
-// import FocusRoom from "./components/FocusRoom.jsx";
+// src/App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage.jsx';
+import Auth from './components/Auth.jsx';
+import ChatTutor from './components/ChatTutor.jsx'; // 1. Import it
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/auth" element={<Auth />} /> */}
-      {/* <Route path="/dashboard" element={<ChatTutor />} /> */}
-      {/* <Route path="/focus" element={<FocusRoom />} /> */}
+      <Route path="/auth" element={<Auth />} />
+      {/* 2. Add the dashboard route */}
+      <Route path="/dashboard" element={<ChatTutor />} /> 
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
