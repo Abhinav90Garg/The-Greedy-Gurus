99ca519cbdfc667022ad3d5fff2fe3fdf710ee3c
Abhinav90Garg <abhinavgarg9011@gmail.com>
2026-04-06 13:10:35 +0530

    test api and ask endpoint

--- backend_py/main.py
+++ backend_py/main.py
@@ -12,11 +12,26 @@ class Query(BaseModel):
 def home():
     return {"message": "ChatTutor backend running"}
 
+import requests
+
 @app.post("/ask")
 def ask(query: Query):
     conversation.append({"role": "user", "content": query.question})
 
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "llama3",
+            "prompt": query.question,
+            "stream": False
+        }
+    )
+
+    answer = response.json()["response"]
+
+    conversation.append({"role": "assistant", "content": answer})
+
     return {
-        "answer": "Backend working",
+        "answer": answer,
         "history": conversation
     }
